936a4254d3c68fd0b884b1117360c38e78bfc596
Son Nguyen <001.IcedTea@gmail.com>
2018-09-29 14:46:04 +0700

    layout

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,14 +5,38 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container-fluid">
+        <div className="justify-content-center d-flex">
+          <h1 className="text-center font-weight-normal my-4">Translate</h1>
+        </div>
+
+        <div className="row body-box p-2">
+          <div className="col-md-6 main-container p-3">
+            <label htmlFor="source" className="">Source language:</label>
+            <div>
+              <div className="d-flex justify-content-between">
+                <select name="source" id="source" className="form-control col-md-3 ">
+                  <option value="123">123</option>
+                  <option value="124">124</option>
+                </select>
+                <button className="btn btn-success col-md-3">Translate</button>
+              </div>
+            </div>
+
+            <textarea name="sourceText" id="sourceText" cols="30" rows="10" placeholder="Enter some text to translate" className="w-100 mt-3 border-0 p-2"></textarea>
+          </div>
+
+          <div className="col-md-6 main-container p-3">
+            <label htmlFor="target" className="">Target language:</label>
+            <select name="target" id="target" className="form-control col-md-3">
+              <option value="123">123</option>
+              <option value="124">124</option>
+            </select>
+
+            <textarea name="sourceText" id="sourceText" cols="30" rows="10" placeholder="" readOnly className="w-100 mt-3 border-0 p-2  "></textarea>
+          </div>
+          <div className="col-md-6"></div>
+        </div>
       </div>
     );
   }
